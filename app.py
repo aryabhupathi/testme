901b9eb5c1d22afd8bc1df6ac33e2db7ce3b05ae
aryabhupathi <108666879+aryabhupathi@users.noreply.github.com>
2026-05-22 23:54:59 +0530

    try

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def home():
     return "Railway Flask App Working!"
 
+port = int(os.environ.get("PORT", 8080))
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
